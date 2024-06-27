a456da2190e67b43efd200525bfe4b8e8a3c6703
Sara Lins <saralins19@gmail.com>
2024-06-27 03:24:24 -0300

    add quick message component

---
+++ src/components/Header/QuickMessage/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: var(--red);
+  color: var(--gray-02);
+  height: 2.5rem;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  strong {
+    font-weight: bold;
+  }
+`
