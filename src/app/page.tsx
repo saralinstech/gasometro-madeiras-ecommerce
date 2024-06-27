0a95b4adc34949a52c6100c73f61bede46815123
Sara Lins <saralins19@gmail.com>
2024-06-27 12:02:03 -0300

    add benefits component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Benefits from '@/components/Benefits'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Benefits />
       <p>oi mundo</p>
     </main>
   )
