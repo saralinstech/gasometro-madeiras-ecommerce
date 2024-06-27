06d3cbe70cfd363734bb4adbb998cc4244ee2c91
Sara Lins <saralins19@gmail.com>
2024-06-27 14:53:29 -0300

    add FullScreenBanner component on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Benefits from '@/components/Benefits'
 import styles from './page.module.css'
 import Categories from '@/components/Categories'
+import FullScreenBanner from '@/components/Banners/FullScreenBanner'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Benefits />
       <Categories />
+      <FullScreenBanner />
       <p>oi mundo</p>
     </main>
   )
