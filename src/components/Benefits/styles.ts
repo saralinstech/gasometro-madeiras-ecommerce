e6aca73609d660b8ffcf61dc8bbd11ac68a4ceba
Sara Lins <saralins19@gmail.com>
2024-06-27 12:01:39 -0300

    create benefits component

---
+++ src/components/Benefits/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  height: 3.25rem;
+  width: 100%;
+
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`
+
+export const BenefitsOptions = styled.span`
+  height: 1.5rem;
+
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`
