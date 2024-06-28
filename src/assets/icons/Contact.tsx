1ddda5c33cbde5418d4bc2023f8457ad46df95d4
Sara Lins <saralins19@gmail.com>
2024-06-28 12:04:10 -0300

    add icons

---
+++ src/assets/icons/Contact.tsx
@@ -0,0 +1,51 @@
+export const SmsIcon = () => {
+  return (
+    <svg
+      width="48"
+      height="24"
+      viewBox="0 0 48 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17.5 20.5H7.5C4.5 20.5 2.5 19 2.5 15.5V8.5C2.5 5 4.5 3.5 7.5 3.5H17.5C20.5 3.5 22.5 5 22.5 8.5V15.5C22.5 19 20.5 20.5 17.5 20.5Z"
+        stroke="#595959"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M17.5 9L14.37 11.5C13.34 12.32 11.65 12.32 10.62 11.5L7.5 9"
+        stroke="#595959"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  )
+}
+export const LocationIcon = () => {
+  return (
+    <svg
+      width="48"
+      height="24"
+      viewBox="2 0 30 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ marginRight: '1rem' }}
+    >
+      <path
+        d="M12.4989 13.4299C14.222 13.4299 15.6189 12.0331 15.6189 10.3099C15.6189 8.58681 14.222 7.18994 12.4989 7.18994C10.7758 7.18994 9.37891 8.58681 9.37891 10.3099C9.37891 12.0331 10.7758 13.4299 12.4989 13.4299Z"
+        stroke="#595959"
+        stroke-width="1.5"
+      />
+      <path
+        d="M4.12166 8.49C6.09166 -0.169998 18.9217 -0.159997 20.8817 8.5C22.0317 13.58 18.8717 17.88 16.1017 20.54C14.0917 22.48 10.9117 22.48 8.89166 20.54C6.13166 17.88 2.97166 13.57 4.12166 8.49Z"
+        stroke="#595959"
+        stroke-width="1.5"
+      />
+    </svg>
+  )
+}
