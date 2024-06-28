6835f04fbfb788a1352c5d244db8a2bdd57f891e
Sara Lins <saralins19@gmail.com>
2024-06-28 17:12:54 -0300

    add products list

---
+++ src/utils/index.ts
@@ -0,0 +1,34 @@
+export const products = [
+  {
+    image: '/products/prod-1.png',
+    altImage: '',
+    name: 'MDF BP 02 Faces Sense Off White Suave 2.750 x 1.850 x 18mm Duratex',
+    previousPrice: 'R$ 227,42',
+    currentPrice: 'R$ 151,20',
+    installments: 'ou 10x de R$ 99,00',
+  },
+  {
+    image: '/products/prod-3.png',
+    altImage: '',
+    name: 'MDF BP 02 Faces Sense Off White Suave 2.750 x 1.850 x 18mm Duratex',
+    previousPrice: 'R$ 227,42',
+    currentPrice: 'R$ 151,20',
+    installments: 'ou 10x de R$ 99,00',
+  },
+  {
+    image: '/products/prod-2.png',
+    altImage: '',
+    name: 'MDF BP 02 Faces Sense Off White Suave 2.750 x 1.850 x 18mm Duratex',
+    previousPrice: 'R$ 227,42',
+    currentPrice: 'R$ 151,20',
+    installments: 'ou 10x de R$ 99,00',
+  },
+  {
+    image: '/products/prod-4.png',
+    altImage: '',
+    name: 'MDF BP 02 Faces Sense Off White Suave 2.750 x 1.850 x 18mm Duratex',
+    previousPrice: 'R$ 227,42',
+    currentPrice: 'R$ 151,20',
+    installments: 'ou 10x de R$ 99,00',
+  },
+]
